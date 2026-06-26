da6b0d6d62e5edb8104877ae66d7c81256e323cf
João <briel.fariasj@gmail.com>
2026-06-25 21:14:39 -0300

    estrutura inicial da API

---
+++ src/main/java/br/com/trabalho/chat/TrabalhoIaApplication.java
@@ -0,0 +1,13 @@
+package br.com.trabalho.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/trabalho/chat/TrabalhoIaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.trabalho.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
